49c8f5aa3e489a68cd154b0c461ef0a707a8c7a5
J Franco Ray <kean.franco07@gmail.com>
2016-11-26 23:03:34 +0800

    added login page, filters for app table

--- hackathon/app/controllers/performance_controller.rb
+++ hackathon/app/controllers/performance_controller.rb
@@ -4,6 +4,9 @@ class PerformanceController < ApplicationController
     @chart1 = LazyHighCharts::HighChart.new('graph') do |f|
       # f.title(text: "Activity for the past few days")
       f.xAxis(categories: ["Nov 21", "Nov 22", "Nov 23", "Nov 24", "Nov 25"])
+      @statuses.each do |v, k|
+        # f.series(name: v.name, yAxis: k, data: [14119, 5068, 4985, 3339, 2656])
+      end
       f.series(name: "GDP in Billions", yAxis: 0, data: [14119, 5068, 4985, 3339, 2656])
       f.series(name: "Population in Millions", yAxis: 0, data: [310, 127, 1340, 81, 65])
 
@@ -12,52 +15,58 @@ class PerformanceController < ApplicationController
       ]
 
       # f.legend(align: 'center', verticalAlign: 'top', y: 75, x: -50, layout: 'vertical')
-      f.chart({defaultSeriesType: "column"})
+      # f.chart({defaultSeriesType: "column"})
     end
 
     @chart2 = LazyHighCharts::HighChart.new('graph') do |f|
-      # f.title(text: "Activity for the past few weeks")
-      f.xAxis(categories: ["United States", "Japan", "China", "Germany", "France"])
+      # f.title(text: "Activity for the past few days")
+      f.xAxis(categories: ["Nov 21", "Nov 22", "Nov 23", "Nov 24", "Nov 25"])
+      @statuses.each do |v, k|
+        # f.series(name: v.name, yAxis: k, data: [14119, 5068, 4985, 3339, 2656])
+      end
       f.series(name: "GDP in Billions", yAxis: 0, data: [14119, 5068, 4985, 3339, 2656])
-      f.series(name: "Population in Millions", yAxis: 1, data: [310, 127, 1340, 81, 65])
+      f.series(name: "Population in Millions", yAxis: 0, data: [310, 127, 1340, 81, 65])
 
       f.yAxis [
-        {title: {text: "GDP in Billions"} },
-        {title: {text: "Population in Millions"}, opposite: true},
+        {title: {text: "no of applicants"}, plotLines: [{value: 0, width: 1, color: '#808080'}] }
       ]
 
-      # f.legend(align: 'right', verticalAlign: 'top', y: 75, x: -50, layout: 'vertical')
-      f.chart({defaultSeriesType: "column"})
+      # f.legend(align: 'center', verticalAlign: 'top', y: 75, x: -50, layout: 'vertical')
+      # f.chart({defaultSeriesType: "column"})
     end
 
     @chart3 = LazyHighCharts::HighChart.new('graph') do |f|
-      # f.title(text: "Activity for the past few months")
-      f.xAxis(categories: ["United States", "Japan", "China", "Germany", "France"])
+      # f.title(text: "Activity for the past few days")
+      f.xAxis(categories: ["Nov 21", "Nov 22", "Nov 23", "Nov 24", "Nov 25"])
+      @statuses.each do |v, k|
+        # f.series(name: v.name, yAxis: k, data: [14119, 5068, 4985, 3339, 2656])
+      end
       f.series(name: "GDP in Billions", yAxis: 0, data: [14119, 5068, 4985, 3339, 2656])
-      f.series(name: "Population in Millions", yAxis: 1, data: [310, 127, 1340, 81, 65])
+      f.series(name: "Population in Millions", yAxis: 0, data: [310, 127, 1340, 81, 65])
 
       f.yAxis [
-        {title: {text: "GDP in Billions", margin: 70} },
-        {title: {text: "Population in Millions"}, opposite: true},
+        {title: {text: "no of applicants"}, plotLines: [{value: 0, width: 1, color: '#808080'}] }
       ]
 
-      # f.legend(align: 'right', verticalAlign: 'top', y: 75, x: -50, layout: 'vertical')
-      f.chart({defaultSeriesType: "column"})
+      # f.legend(align: 'center', verticalAlign: 'top', y: 75, x: -50, layout: 'vertical')
+      # f.chart({defaultSeriesType: "column"})
     end
 
     @chart4 = LazyHighCharts::HighChart.new('graph') do |f|
-      # f.title(text: "Activity for the past few quarter")
-      f.xAxis(categories: ["United States", "Japan", "China", "Germany", "France"])
+      # f.title(text: "Activity for the past few days")
+      f.xAxis(categories: ["Nov 21", "Nov 22", "Nov 23", "Nov 24", "Nov 25"])
+      @statuses.each do |v, k|
+        # f.series(name: v.name, yAxis: k, data: [14119, 5068, 4985, 3339, 2656])
+      end
       f.series(name: "GDP in Billions", yAxis: 0, data: [14119, 5068, 4985, 3339, 2656])
-      f.series(name: "Population in Millions", yAxis: 1, data: [310, 127, 1340, 81, 65])
+      f.series(name: "Population in Millions", yAxis: 0, data: [310, 127, 1340, 81, 65])
 
       f.yAxis [
-        {title: {text: "GDP in Billions", margin: 70} },
-        {title: {text: "Population in Millions"}, opposite: true},
+        {title: {text: "no of applicants"}, plotLines: [{value: 0, width: 1, color: '#808080'}] }
       ]
 
-      # f.legend(align: 'right', verticalAlign: 'top', y: 75, x: -50, layout: 'vertical')
-      f.chart({defaultSeriesType: "column"})
+      # f.legend(align: 'center', verticalAlign: 'top', y: 75, x: -50, layout: 'vertical')
+      # f.chart({defaultSeriesType: "column"})
     end
   end
 end
